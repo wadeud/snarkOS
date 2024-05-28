e36a80b3d31aa63dd6a7c28cdbcc048c99addeec
wadeud <161763388+wadeud@users.noreply.github.com>
2024-05-28 16:12:17 +0100

    Update lib.rs
    Signed-off-by: wadeud <161763388+wadeud@users.noreply.github.com>

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,22 +1,32 @@
-// Copyright (C) 2019-2023 Aleo Systems Inc.
-// This file is part of the snarkOS library.
-
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at:
-// http://www.apache.org/licenses/LICENSE-2.0
-
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Copyright headers...
 
 #![forbid(unsafe_code)]
-#![recursion_limit = "256"]
 
 #[macro_use]
 extern crate thiserror;
 
 pub mod commands;
 pub mod helpers;
+
+/// Errors that can be encountered in this crate
+pub mod error {
+    use thiserror::Error;
+
+    #[derive(Error, Debug)]
+    pub enum MyError {
+        #[error("An error occurred: {0}")]
+        GeneralError(String),
+        #[error("Failed due to IO error")]
+        IOError(#[from] std::io::Error),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_some_functionality() {
+        assert_eq!(2 + 2, 4);
+    }
+}
